fffbb1b06719276f11b021640087bd577f5ba3e5
Guilherme Lacerda <guilhermeslacerda@gmail.com>
2024-10-03 22:05:27 -0300

    adding unit test and domain classes

---
+++ tests/main/HelloTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloTest {
+
+	@Test
+	void testGettHello() {
+		HelloWorld hw = new HelloWorld();
+	}
+
+}
