591b46b50d2817abf5d21a3cdfa5db7acb58ccb6
DLazaroSPS <douglas.pires@animaeducacao.com.br>
2024-10-19 09:41:39 -0300

    [ADD] others tests types & [DOC] all test methods

--- tests/main/HelloTest.java
+++ tests/main/HelloTest.java
@@ -4,29 +4,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Classe de teste para a classe HelloWorld.
+ */
 class HelloTest {
 
-	@Test
-	void testGetHello() {
-		HelloWorld hw = new HelloWorld("José");	
-		assertEquals("Hello world, José", hw.getHello());
-	}
+    /**
+     * Testa o método getHello() com o nome "José".
+     */
+    @Test
+    void testGetHello() {
+        HelloWorld hw = new HelloWorld("José");
+        assertEquals("Hello world, José", hw.getHello());
+    }
 
-	@Test
-	void testGetHelloWithDifferentName() {
-		HelloWorld hw = new HelloWorld("Maria");    
-		assertEquals("Hello world, Maria", hw.getHello());
-	}
+    /**
+     * Testa o método getHello() com um nome diferente, "Maria".
+     */
+    @Test
+    void testGetHelloWithDifferentName() {
+        HelloWorld hw = new HelloWorld("Maria");
+        assertEquals("Hello world, Maria", hw.getHello());
+    }
 
-	@Test
-	void testGetHelloWithEmptyName() {
-		HelloWorld hw = new HelloWorld("");    
-		assertEquals("Hello world, ", hw.getHello());
-	}
+    /**
+     * Testa o método getHello() com um nome vazio.
+     */
+    @Test
+    void testGetHelloWithEmptyName() {
+        HelloWorld hw = new HelloWorld("");
+        assertEquals("Hello world, ", hw.getHello());
+    }
 
-	@Test
-	void testGetHelloWithNullName() {
-		HelloWorld hw = new HelloWorld(null);    
-		assertEquals("Hello world, null", hw.getHello());
-	}
+    /**
+     * Testa o método getHello() com um nome nulo.
+     */
+    @Test
+    void testGetHelloWithNullName() {
+        HelloWorld hw = new HelloWorld(null);
+        assertEquals("Hello world, null", hw.getHello());
+    }
+
+    /**
+     * Testa o método getHello() com um nome inesperado para garantir que a saída não seja igual a um valor específico.
+     */
+    @Test
+    void testGetHelloWithUnexpectedName() {
+        HelloWorld hw = new HelloWorld("Carlos");
+        assertNotEquals("Hello world, Maria", hw.getHello());
+    }
 }
